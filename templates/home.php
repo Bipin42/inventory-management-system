cc96e8b7376395709ddbb3483ff85817631b511b
Bipin42 <77967039+Bipin42@users.noreply.github.com>
2023-03-18 12:50:53 +0545

    Add files via upload

---
+++ templates/home.php
@@ -0,0 +1,5 @@
+	  <div class="tab-pane fade active show" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-vendor-tab">
+				<div class="card card-outline-secondary my-4">
+<marquee class="content">Welcome <?php echo $_SESSION['fullName']; ?></marquee>
+				</div>
+			  </div>
